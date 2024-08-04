45639ed49a20ba05d2a5df779e1d6ebc6cb8da29
FedorKhovanov <102238235+FedorKhovanov@users.noreply.github.com>
2024-08-04 21:18:18 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,7 @@
 import pyperclip
 import random
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk
 
 def random_number(min_value, max_value):
     pre_result_number = random.randint(min_value, max_value - 1)
@@ -15,12 +15,36 @@ def show_result():
     generated_number = random_number(10**16, 10**17)
     result_label.config(text=generated_number)
     copy_to_clipboard(generated_number)
-    
+
+def show_about():
+    about_text = """
+    Чтобы не попасть в ситуацию ХВГ (хотя вероятность генерации одинаковых SSSC менее чем 0.001%) используйте на свой страх и риск)))
+    Благодарность в жидкой валюте можно выразить сюда -> Fedor.Khovanov@pepsico.com
+    """
+    about_window = tk.Toplevel(root)
+    about_window.title("Инфо")
+    about_label = tk.Label(about_window, text=about_text, font=("Arial", 10), justify="left", padx=20, pady=20) # Шрифт уменьшен
+    about_label.pack()
 
 # Создание GUI
 root = tk.Tk()
-root.title("SSSC Generator")
+root.title("SSSC Генератор")
 
+# Задание размера окна
+window_width = 400
+window_height = 200
+root.geometry(f"{window_width}x{window_height}")
+
+# Создание меню
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+
+# Добавление меню About
+about_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Меню", menu=about_menu)
+about_menu.add_command(label="Инфо", command=show_about)
+
+# Основное окно
 result_label = tk.Label(root, text="", font=("Arial", 16))
 result_label.pack(pady=20)
 
